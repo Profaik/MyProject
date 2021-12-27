be2fce49a51a699cc349e8aac24aba21ec5ff19d
Profaik <messi2805@mail.ru>
2021-12-27 21:09:53 +0300

    Commit for the Submit form

--- my/core/models.py
+++ my/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.forms import forms
+from django.contrib.auth.models import User
 # Create your models here.
+class User(models.Model):
+    first_name=models.CharField(max_lenght=100)
+    last_name=models.CharFeild(max_lenght=100)
+    password=models.IntegerField(max_lenght=100)
+    def get_participants(self):
+        return EventUser.objects.filter(event=self)
+class Sport_company(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True,)
+    age = models.IntegerField(max_lenght=100)
+    city = models.CharField(max_length=100)
+    sport = models.IntegerField(max_length=100)
+    userId = models.IntegerField(max_length=100)
+    weight = models.FloatField(max_length= 100)
--- my/core/tests.py
+++ my/core/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
-
+from core.models import User, Sport_company
+from django.contrib.auth.models import User
+# Create your tests here.
+class UserTest(TestCase):
+    def test_event_model_create(self):
+        user = User.objects.create(username="Test") # Создадим пользователя
+        # он будет автором события
+        participant1 = User.objects.create(username="Участник события №1")
+        participant2 = User.objects.create(username="Участник события №2")
+        newEvent = User.objects.create(
+            name="Тестовое событие",
+            game=1,
+            date="2021-12-13 16:20",
+            author=user,
+            description="Test",
+            status=1,
+            recording="https://test.com"
+        )
+        self.assertEqual(newEvent.name, "Тестовое событие")
+        eventUser1 = Sport_company.objects.create(event=newEvent, user=participant1)
+        eventUser2 = Sport_company.objects.create(event=newEvent, user=participant2)
+        self.assertEqual(newEvent.get_participants()[0].user, participant1) # проверяем что участник 1 является участником события
+        self.assertEqual(newEvent.get_participants()[1].user, participant2) # проверяем что участник 2 является участником события
 # Create your tests here.
